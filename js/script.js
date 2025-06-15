cbdb36fe3e7d2b376ce4f8725ea401e9cc77787d
RenanSilvaOliveira968 <Renansilvaoli968@gmail.com>
2025-06-15 17:43:34 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function NomeUniversidade() {
+    return (
+      <p>
+        <b>O Instituto Federal de Ciência e Tecnologia de São Paulo - </b> é uma autarquia federal de ensino.
+        <br></br>
+        <br></br>
+        Fundada em 1909, como escola de aprendizer artifices,é reconhecida pela sociedade paulista por sua excelência no ensino publico gratuito de qualidade.
+        <br></br>
+        <br></br>
+        Durante sua história, recebeu, também, os nomes de escola tecnica feral de são paulo e centro federal de educação tecnologica de são paulo. com a transformação em instituto, em dezembro de 2008, passou a ter relevância
+        de universidade, destacando-se pela autonomia.
+        <br></br>
+        <br></br>
+        Com a mudança o instituto federal de são paulo passou a destinar 50% de vagas para os cursos tecnicos e, no minimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de ciência e da matematica.
+        completamente, continuara oferecendo cursos de formação inicial e continuada, tecnologias, engenharia e pós-graduação.
+        <br></br>
+        <br></br>
+        Além do cursos presenciais, o instituto federal de são paulo oferece os cursos tecnicos em administração e informatica para internet e, a partir de 2012, o superior de formação de professores na modalide de ensino a distânica (EAD)
+        <br></br>
+        <br></br>
+        O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuidas pelo estado de São paulo.;
+        <br></br>
+        <br></br>
+        Localizado em <b> Rua Pedro vincente, 625 - Canidé - São Paulo - SP - Brasil - CEP: 01109-010</b>
+
+
+      </p>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<NomeUniversidade />);
+  
